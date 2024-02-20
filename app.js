f8d35f2877aa0b3391621b0dbcbfe7f52dac1e05
basilkhan1250 <basilkhan1250@gmail.com>
2024-02-20 14:31:45 -0800

    chapter 1-2-3-4-5

---
+++ app.js
@@ -0,0 +1,265 @@
+// chapter 1  Question 1 
+
+// var greet = "Hello! Visitor how are you doing?"
+// alert(greet)
+
+
+// chapter 1 Question 2
+
+// var error = "Error! Please enter a valid password"
+// alert(error)
+
+
+// chapter 1 Question 3
+
+// var welcome = "Welcome to JS land... \nHappy Coding"
+// alert(welcome)
+
+
+// chapter 1 Question 4
+
+// var welcome = "Welcome to JS land..."
+// alert(welcome)
+// var code = "Happy Coding!"
+// alert(code)
+
+
+// chapter 1 Question 5 will run in browser's console
+
+
+
+
+// chapter 2 Question 1
+
+// var username;
+
+
+// chapter 2 Question 2
+
+// var myName = "Basil khan"
+
+
+// chapter 2 Question 3
+
+// var message = " Hello World"
+// alert(message)
+
+
+// chapter 2 Question 4 
+
+// var fullName = "John Doe"
+// var age = "15"
+// var profession = "Certified Mobile App Development"
+// alert(fullName)
+// alert(age)
+// alert(profession)
+
+
+// chapter 2 Question 5
+
+// var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+// alert(pizza)
+
+
+// chapter 2 Question 6
+
+// var email = "basilkhan1250@gmail.com"
+// alert("My email address is " + email)
+
+
+// chapter 2 Question 7
+
+// var book = "A Smarter Way To Learn Javascript"
+// alert("I'm trying to learn form the book " + book)
+
+
+// chapter 2 Question 8
+
+// var script = "Yah! I can write HTML content through Javascript"
+// document.write(script)
+
+
+// chapter 2 Question 9
+
+// var symbol = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(symbol)
+
+// chapter 3 Question 1
+
+// var age = 19
+// alert("I'm " + age + " years old")
+
+
+// chapter 3 Question 2
+
+// var visits = "You have visited this site 14 times"
+// alert(visits)
+
+
+// chapter 3 Question 3
+
+// var birthYear = 2004
+// document.write("My birth year is " + birthYear) 
+// document.write( "<br/>" + "Data type of my declared variable is " + typeof(birthYear))
+
+
+// chapter 3 Question 4
+
+// var user = prompt("enter you name")
+// var product = prompt("how many shirts you want")
+// var store = "XYZ-Clothingstore"
+// document.write(user + " ordered " + product + " on " + store )
+
+
+// chapter 4 question 1 
+
+
+// var head = "<h1>Rules of naming JS variables</h1>"
+// var num = "numbers"
+// var dollar = "$"
+// var dash = "_"
+// document.write(head)
+// document.write("variable names can only contain , " + num + "," + dollar + " and " + dash + " . " + "For Example : " + dollar + "my" + dash + "1stVariable <br/>" ) 
+// document.write("variable must begin with a letter , " + dollar + " or " + dash + " . " + "For Example : " + dollar + "name, " + dash + "name or name <br/>")
+// document.write("variables are case sensitive <br/>")
+// document.write("variable names should not be JS keywords")
+
+
+// chapter 5 question 1 
+
+
+// var num1 = 3
+// var num2 = 5
+// var total = num1 + num2
+// console.log(total)
+
+// chapter 5 question 2
+
+// var num1 = 3
+// var num2 = 5
+// var total = num2 - num1
+// console.log(total)
+
+// var num1 = 3
+// var num2 = 5
+// var total = num1 / num2
+// console.log(total)
+
+// var num1 = 3
+// var num2 = 5
+// var total = num1 % num2
+// console.log(total)
+
+// chapter 5 question 3
+
+
+// document.write("Value after variable declaration is undifined <br/> ")
+// var num = 5
+// document.write("initial value : " + num + "<br/>")
+// var increment = ++num
+// document.write("Value after increment is : " + increment + "<br/>")      // 6
+// var addition = increment + 7   
+// document.write("Value after addition is : " + addition + "<br/>")       // 13
+// var decrement = --addition
+// document.write("Value after decrement is : " + decrement + "<br/>")      // 12
+// var remainder = decrement % 3
+// document.write("The remainder is : " + remainder)      // 0
+
+// chapter 5 Question 4 
+
+// var ask = +prompt("how many tickets you want?")
+// var ticketPrice = 600
+// var totalPrice = ticketPrice * ask
+// document.write("Total cost to buy " + ask + " tickets to a movie is " + totalPrice + "PKR")
+
+// chapter 5 question 5 
+
+// var tableNumber = +prompt("which number's table you want?")
+// document.write( tableNumber + " x 1 = " + tableNumber * 1 + "<br/>")
+// document.write( tableNumber + " x 2 = " + tableNumber * 2 + "<br/>")
+// document.write( tableNumber + " x 3 = " + tableNumber * 3 + "<br/>")
+// document.write( tableNumber + " x 4 = " + tableNumber * 4 + "<br/>")
+// document.write( tableNumber + " x 5 = " + tableNumber * 5 + "<br/>")
+// document.write( tableNumber + " x 6 = " + tableNumber * 6 + "<br/>")
+// document.write( tableNumber + " x 7 = " + tableNumber * 7 + "<br/>")
+// document.write( tableNumber + " x 8 = " + tableNumber * 8 + "<br/>")
+// document.write( tableNumber + " x 9 = " + tableNumber * 9 + "<br/>")
+// document.write( tableNumber + " x 10 = " + tableNumber * 10 + "<br/>")
+
+// chapter 5 Questin 6
+
+// var Celsius = 25
+// var Fahrenheit = 70
+// var CelsiusToFahrenheit = (Fahrenheit - 32) * 5 / 9
+// var fahrenheitToCelsuis = (Celsius * 9 / 5) + 32
+// document.write(Fahrenheit +"fahrenheit is " + CelsiusToFahrenheit + "celsius <br/>") 
+// document.write(Celsius +"celsius is " + fahrenheitToCelsuis + "fahrenheit") 
+
+// chapter 5 Question 7
+
+// var itemPrice1 = 650
+// var itemPrice2 = 100
+// var quantityItem1 = 3
+// var quantityItem2 = 7
+// var sCharges = 100
+
+// var total = itemPrice1 * quantityItem1 + itemPrice2 * quantityItem2 + sCharges
+// document.write("price of item 1 is " + itemPrice1 + "<br/>")
+// document.write("Quantity of item 1 is " + quantityItem1 + "<br/>")
+// document.write("price of item 2 is " + itemPrice2 + "<br/>")
+// document.write("Quantity of item 2 is " + quantityItem2 + "<br/>")
+// document.write("Shipping charges " + sCharges + "<br/>")
+// document.write("Total cost of your order is " + total )
+
+// chapter 5 question 8
+
+
+// var totalMarks = 980
+// var obtainedMarks = 804
+// var percentage = 100 * obtainedMarks /totalMarks
+// document.write("Total marks: " +totalMarks + "<br/>")
+// document.write("obtained marks: "+obtainedMarks + "<br/>")
+// document.write("Percentage: "+percentage + "<br/>")
+
+// chapter 5 Question 9
+
+// var dollar = 104.80
+// var riyal= 28
+// var pkrCurrency = dollar * 10 + riyal * 25
+// console.log(pkrCurrency)
+
+// chapter 5 question 10
+
+// var value = 5
+// var arithmetic = value + 5 * 10 / 2
+// console.log(arithmetic)
+
+// chapter 5 question 11
+
+// var currentYear = 2024
+// var birthYear = 2004
+// var age = currentYear - birthYear
+// document.write("current year: "+currentYear +"<br/>")
+// document.write("birth year year: "+birthYear +"<br/>")
+// document.write("your age: "+age +"<br/>")
+
+// chapter 5 question 12
+
+
+
+
+
+
+// chapter 5 question 13
+
+
+// var favSnacks = "lays"
+// var currentAge = 15
+// var eAge = 65
+// var amountOfSPD = 3
+// var supply = ( eAge - currentAge ) * amountOfSPD
+// document.write("Favourite Snacks: " + favSnacks + "<br/>")
+// document.write("current age: "+ currentAge + "<br/>")
+// document.write("Estimated Maximum age: " + eAge + "<br/>")
+// document.write("Amount of snacks per day: " + amountOfSPD + "<br/>")
+// document.write("You will need " + supply + favSnacks + " to last you until the ripe old age of " + eAge)
